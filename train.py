cc911861b8594085ba3d79c8e6a03fe619d27326
Dx-by-Dy <alsmirnov2004@gmail.com>
2024-01-02 17:42:08 +0300

    test

--- train.py
+++ train.py
@@ -8,16 +8,18 @@ from dvclive import Live
 df = pd.read_csv('iris_data.csv')
 x_train, x_test, y_train, y_test = train_test_split(df.drop(["target"], axis=1), df["target"], test_size=0.5, random_state=0)
 
-clf = MLPClassifier(hidden_layer_sizes=(50, 10), max_iter=1000, random_state=42)
-clf.fit(x_train, y_train)
-
-y_pred = clf.predict(x_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")
-
-LC = clf.loss_curve_
-
-data = np.column_stack((range(len(LC)), LC))
+config = [(50, 10), (100, 10), (10, 10)]
 
 with Live(save_dvc_exp=True) as live:
-    live.log_plot(Live, "MLP_50_10", data, "0", "1")
+    for conf in config:
+        clf = MLPClassifier(hidden_layer_sizes=conf, max_iter=1000, random_state=42)
+        clf.fit(x_train, y_train)
+
+        LC = clf.loss_curve_
+        data = np.column_stack((range(len(LC)), LC))
+
+        y_pred = clf.predict(x_test)
+        accuracy = round(accuracy_score(y_test, y_pred), 2)
+
+        name = f"MPL{conf[0], conf[1]}_{accuracy}"
+        live.log_plot(name, data, "0", "1", "linear", name, "epochs", "loss")
